0034c852d552d540554c39014c6979587c0e9e04
Gourab Banik <gourabbanik2399@gmail.com>
2021-09-29 14:05:06 +0530

    Middleware add

--- server/app.js
+++ server/app.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const app = express();
 const PORT = 5000
-app.get('/', function(req, res) {
+
+const customMiddleware = (req, res, next)=>{
+    console.log("Middleware executed");
+    next();
+}
+
+// app.use(customMiddleware)
+
+//Middleware not use
+app.get('/', (req, res) =>{
+    console.log("home")
     res.send("Hello world!");
 });
+
+//Middleware use
+app.get('/about',customMiddleware, (req, res) =>{
+    console.log("about")
+    res.send("about world!");
+});
+
+
 
 app.listen(PORT, () => {
     console.log("server is running on port ", PORT)
